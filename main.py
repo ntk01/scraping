d68046ce21eb27ffd84528c5d8615c3ee47daa2a
ntk01 <neuron.futuring1024@gmail.com>
2019-10-16 15:37:12 +0900

    run

---
+++ main.py
@@ -0,0 +1,33 @@
+import flask
+from flask import render_template
+from flask import request
+import describe
+import scrape
+
+
+app = flask.Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/chart', methods=['GET', 'POST'])
+def chart():
+    q = request.form.get('keyword')
+
+    labels, values = describe.get_word(q)
+    labels2, values2 = scrape.get_co_occur(q)
+
+    legend = "Description Words"
+    legend2 = "Web Page Words"
+    labels = labels
+    values = values
+    labels2 = labels2
+    values2 = values2
+    return render_template('chart.html', labels=labels, values=values, legend=legend, legend2=legend2, labels2=labels2, values2=values2)
+
+
+if __name__ == '__main__':
+    app.run()
