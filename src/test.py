ee30e922c9c0a3daf2de1f48faaf8bcd1b44b17b
ntk01 <n.taku.law@gmail.com>
2021-01-25 11:11:25 +0900

    conf

--- src/test.py
+++ src/test.py
@@ -2,13 +2,13 @@ import scrape
 import describe
 
 
-def test_scrape():
-    keys, values = scrape.get_co_occur("test")
-    try:
-        (len(keys) == 10) and (len(values) == 10)
-        print("OK")
-    except ValueError:
-        print("Unexpected error")
+# def test_scrape():
+#     keys, values = scrape.get_co_occur("test")
+#     try:
+#         (len(keys) == 10) and (len(values) == 10)
+#         print("OK")
+#     except ValueError:
+#         print("Unexpected error")
 
 
 def test_describe():
