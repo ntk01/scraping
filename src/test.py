947b1fc199fbe38315a64283b20b460cbf7f7088
ntk01 <n.taku.law@gmail.com>
2023-06-18 19:35:03 +0900

    ci

--- src/test.py
+++
@@ -1,9 +0,0 @@
-import describe
-
-def test_describe():
-    keys, values = describe.get_word("test")
-    try:
-        (len(keys) == 10) and (len(values) == 10)
-        print("OK")
-    except ValueError:
-        print("Unexpected error")
